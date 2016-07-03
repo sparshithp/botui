00b433394d66f6d0ee074d389396dbe9d7e92640
Sparshith <sparsh.pro@gmail.com>
2016-07-03 01:20:48 -0700

    Process env

--- app/server.js
+++ app/server.js
@@ -5,5 +5,5 @@ var app = connect();
 
 app.use(serveStatic("."));
 app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode", this.address().port);
 });
