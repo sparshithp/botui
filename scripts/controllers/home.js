62f4c3f7efcbdd2440091334be5f214b30893f18
Sparshith <sparsh.pro@gmail.com>
2016-07-06 00:44:25 -0700

    Login functionality

--- scripts/controllers/home.js
+++ scripts/controllers/home.js
@@ -1,7 +1,14 @@
 /**
  * Created by sparshithp on 6/10/16.
  */
-app.controller('HomeCtrl', function($scope, $alert, $auth, $rootScope) {
+app.controller('HomeCtrl', function($scope, $alert, $auth, $rootScope, $http) {
     $rootScope.title = "Meals";
     console.log($auth.isAuthenticated());
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3000/api/me',
+        skipAuthorization: true // `Authorization: Bearer <token>` will not be sent on this request.
+    }).then(function(res){
+        console.log(res);
+    });
 });
--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -1,21 +1,42 @@
-app.controller('LoginCtrl', function($scope, $alert, $auth, $rootScope) {
+app.controller('LoginCtrl', function ($scope, $alert, $auth, $rootScope) {
     $rootScope.title = "Login";
+    $scope.message="";
     console.log($scope.title);
-    $scope.login = function() {
-      $auth.login({ email: $scope.email, password: $scope.password })
-        .then(function(res) {
-            console.log($auth.isAuthenticated());
-        })
-        .catch(function(response) {
-            console.log(response);
-            $scope.message = "wrong";
-        });
+
+
+    $scope.login = function () {
+
+        var email = $scope.email;
+        var password = $scope.password;
+
+
+        if(!email || !password){
+            $scope.message = "Please fill all fields";
+            return;
+        }
+        email = email.toLowerCase();
+
+
+        $auth.login({email: email, password: password})
+            .then(function (res) {
+                if (res.data && res.data.message) {
+                    $scope.message = res.data.message;
+                }
+            })
+            .catch(function (res) {
+                if (res.data && res.data.message) {
+                    $scope.message = res.data.message;
+                } else {
+                    $scope.message = "Network error. Please try again";
+                }
+            });
     };
-    $scope.authenticate = function(provider) {
-      $auth.authenticate(provider)
-        .then(function(){
-        })
-        .catch(function(response) {
-        });
+
+    $scope.authenticate = function (provider) {
+        $auth.authenticate(provider)
+            .then(function () {
+            })
+            .catch(function (response) {
+            });
     };
-  });
+});
